34702b6eb0b5cc0937ccb779a747960764adbb12
evabat <evabat13@gmail.com>
2019-03-24 18:47:36 +0200

    added main activity buttons and btn internet functionality

--- app/src/main/java/com/eap/sdy61/ge4/eva_b/eapp/MainActivity.java
+++ app/src/main/java/com/eap/sdy61/ge4/eva_b/eapp/MainActivity.java
@@ -1,15 +1,26 @@
 package com.eap.sdy61.ge4.eva_b.eapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+
+    // Buttons
+    Button internetBtn;
+    Button openStorageBtn;
+    Button saveStorageBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
         setSupportActionBar(mToolbar);
+
+        // Get the buttons by the view
+        internetBtn = findViewById(R.id.btnInternet);
+        openStorageBtn = findViewById(R.id.btnOpenStorage);
+        saveStorageBtn = findViewById(R.id.btnSaveStorage);
+
+
+        internetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOnline()) {
+                    Uri uri = Uri.parse("http://www.eap.gr");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Αποτυχία εμφάνισης της σελίδας, παρακολούμε συνδεθείτε στο internet.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -42,16 +72,21 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    public void navToTabs(View view)
-//    {
-//        Intent intent = new Intent(FromActivity.this, ToActivity.class);
-//        startActivity(intent);
-//    }
-//
-//    public void sendMessage(View view)
-//    {
-//        Intent intent = new Intent(FromActivity.this, ToActivity.class);
-//        startActivity(intent);
-//    }
+    public boolean isOnline() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 
 }
