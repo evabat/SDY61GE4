59b469fafe7a623e7fa8e88fc6b46df8e15612a3
evabat <evabat13@gmail.com>
2019-04-07 01:12:50 +0300

    vuforia_first_attempt

--- app/src/main/java/com/eap/sdy61/ge4/eva_b/eapp/ThreeFragment.java
+++ app/src/main/java/com/eap/sdy61/ge4/eva_b/eapp/ThreeFragment.java
@@ -1,31 +1,18 @@
 package com.eap.sdy61.ge4.eva_b.eapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.eap.sdy61.ge4.eva_b.eapp.vuforiaSamples.ImageTargets;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ThreeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ThreeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ThreeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -33,20 +20,9 @@ public class ThreeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ThreeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ThreeFragment newInstance(String param1, String param2) {
         ThreeFragment fragment = new ThreeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,10 +30,6 @@ public class ThreeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -68,12 +40,12 @@ public class ThreeFragment extends Fragment {
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Intent intent = new Intent(getActivity(), ImageTargets.class);
+        startActivity(intent);
     }
+
 
     @Override
     public void onAttach(Context context) {
@@ -92,16 +64,7 @@ public class ThreeFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
